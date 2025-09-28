855639b1b3951b1b8daa830df5a5994c489a745c
amirrezanosrati <103998422+amirrezanosrati@users.noreply.github.com>
2025-09-28 16:05:28 +0330

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,16 +1,15 @@
+# Script.py
 import threading
 import subprocess
-import os
-
-# اجرا همزمان ربات و Flask server
-def run_bot():
-    subprocess.run(["python", "bot.py"])
 
 def run_server():
     subprocess.run(["python", "server.py"])
 
-t1 = threading.Thread(target=run_bot)
-t2 = threading.Thread(target=run_server)
+def run_bot():
+    subprocess.run(["python", "bot.py"])
+
+t1 = threading.Thread(target=run_server)
+t2 = threading.Thread(target=run_bot)
 t1.start()
 t2.start()
 t1.join()
