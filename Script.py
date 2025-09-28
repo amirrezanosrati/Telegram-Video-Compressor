8a0a92abaf7694c2eb0bf011accdc55f111d3fcf
amirrezanosrati <103998422+amirrezanosrati@users.noreply.github.com>
2025-09-28 19:05:35 +0330

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,16 +1,14 @@
 # Script.py
+import asyncio
 import threading
-import subprocess
+from bot import bot
+from server import app
 
-def run_server():
-    subprocess.run(["python", "server.py"])
+def run_flask():
+    app.run(host="0.0.0.0", port=8080)
 
-def run_bot():
-    subprocess.run(["python", "bot.py"])
+# سرور Flask در یک Thread
+threading.Thread(target=run_flask).start()
 
-t1 = threading.Thread(target=run_server)
-t2 = threading.Thread(target=run_bot)
-t1.start()
-t2.start()
-t1.join()
-t2.join()
+# ربات تلگرام
+bot.run()
