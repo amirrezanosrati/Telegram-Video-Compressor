5b17d8f978417e410e68dbb770cc1c744f7ba1f6
amirrezanosrati <103998422+amirrezanosrati@users.noreply.github.com>
2025-09-28 16:05:15 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,20 @@
-from flask import Flask, send_from_directory
+# server.py
 import os
+from flask import Flask, send_from_directory, abort
 
 UPLOAD_FOLDER = "uploads"
 app = Flask(__name__)
 
 @app.route("/<path:filename>")
 def serve_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
+    path = os.path.join(UPLOAD_FOLDER, filename)
+    if not os.path.isfile(path):
+        return abort(404)
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 @app.route("/")
-def home():
-    return "🤖 File Server is Running!"
+def index():
+    return "File server is running."
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
